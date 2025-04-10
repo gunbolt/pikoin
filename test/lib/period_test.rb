02cf925ea484450cb3d8410e9a0df0f442fc8473
stephann <3025661+stephannv@users.noreply.github.com>
2025-04-10 19:38:24 -0300

    Add #range to Period (#10)

--- test/lib/period_test.rb
+++ test/lib/period_test.rb
@@ -1,33 +1,40 @@
 require "test_helper"
 
 class PeriodTest < ActiveSupport::TestCase
-  test "year period" do
+  test "#range" do
     period_2024 = Period.for("2024")
-
-    assert_equal 12, period_2024.steps.size
-    assert_equal Date.new(2024, 1, 1)..Date.new(2024, 1, 31),
-      period_2024.steps.first
-    assert_equal Date.new(2024, 12, 1)..Date.new(2024, 12, 31),
-      period_2024.steps.last
-
     period_2026 = Period.for("2026")
+    period_2025_04 = Period.for("2025-04")
+    period_2025_02 = Period.for("2025-02")
 
-    assert_equal 12, period_2026.steps.size
-    assert_equal Date.new(2026, 1, 1)..Date.new(2026, 1, 31),
-      period_2026.steps.first
-    assert_equal Date.new(2026, 12, 1)..Date.new(2026, 12, 31),
-      period_2026.steps.last
+    assert_equal Date.new(2024, 1, 1)..Date.new(2024, 12, 31),
+      period_2024.range
+    assert_equal Date.new(2026, 1, 1)..Date.new(2026, 12, 31),
+      period_2026.range
+    assert_equal Date.new(2025, 4, 1)..Date.new(2025, 4, 30),
+      period_2025_04.range
+    assert_equal Date.new(2025, 2, 1)..Date.new(2025, 2, 28),
+      period_2025_02.range
   end
 
-  test "month period" do
+  test "#steps" do
+    period_2024 = Period.for("2024")
+    period_2026 = Period.for("2026")
     period_2025_04 = Period.for("2025-04")
-    assert_equal 30, period_2025_04.steps.size
-    assert_equal Date.new(2025, 4, 1), period_2025_04.steps.first
-    assert_equal Date.new(2025, 4, 30), period_2025_04.steps.last
-
     period_2025_02 = Period.for("2025-02")
+
+    assert_equal 12, period_2024.steps.size
+    assert_equal Date.new(2024, 3, 1)..Date.new(2024, 3, 31),
+      period_2024.steps[2]
+
+    assert_equal 12, period_2026.steps.size
+    assert_equal Date.new(2026, 5, 1)..Date.new(2026, 5, 31),
+      period_2026.steps[4]
+
+    assert_equal 30, period_2025_04.steps.size
+    assert_equal Date.new(2025, 4, 10), period_2025_04.steps[9]
+
     assert_equal 28, period_2025_02.steps.size
-    assert_equal Date.new(2025, 2, 1), period_2025_02.steps.first
-    assert_equal Date.new(2025, 2, 28), period_2025_02.steps.last
+    assert_equal Date.new(2025, 2, 19), period_2025_02.steps[18]
   end
 end
