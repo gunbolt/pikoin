0b98cddaca9213a4476704b9cea70b6ac7dd9d2a
stephann <3025661+stephannv@users.noreply.github.com>
2025-04-12 08:30:24 -0300

    Fix sticky header z-index

--- app/components/bolt/page/page_header.rb
+++ app/components/bolt/page/page_header.rb
@@ -10,7 +10,7 @@ module Bolt
       div class: [
         "flex flex-col justify-between gap-4 w-full px-4 bg-base-200 " \
         "lg:flex-row lg:items-center lg:gap-1 py-4",
-        ("top-0 sticky" if @sticky)
+        ("top-0 sticky z-50" if @sticky)
       ], &
     end
   end
