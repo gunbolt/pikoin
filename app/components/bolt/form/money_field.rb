ce62324e3edbb10ecdf665eff0e379790570a573
stephann <3025661+stephannv@users.noreply.github.com>
2025-06-12 18:35:05 -0300

    Setup money field according to the default currency

--- app/components/bolt/form/money_field.rb
+++ app/components/bolt/form/money_field.rb
@@ -15,9 +15,19 @@ module Bolt
         class: "input w-full",
         data: {
           controller: "money-field",
-          money_field_allow_negative_value: @allow_negative.to_s
+          **money_field_settings
         },
         **@attributes
+    end
+
+    def money_field_settings
+      default_currency = Money.default_currency
+      {
+        money_field_allow_negative_value: @allow_negative.to_s,
+        money_field_decimal_separator_value: default_currency.decimal_mark,
+        money_field_thousands_separator_value: default_currency.thousands_separator,
+        money_field_fraction_digits_value: default_currency.exponent
+      }
     end
   end
 end
