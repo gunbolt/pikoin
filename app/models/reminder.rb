076c2db2c0becb6cf25ca678937f2d97ae6d9480
stephann <3025661+stephannv@users.noreply.github.com>
2025-06-14 15:33:16 -0300

    Create reminder and reminder monthly config

---
+++ app/models/reminder.rb
@@ -0,0 +1,24 @@
+class Reminder < ApplicationRecord
+  enum :group, {expense: -1, income: 1}, validate: true
+
+  belongs_to :account
+  belongs_to :category
+
+  delegated_type :config, types: %w[ReminderMonthlyConfig], dependent: :destroy
+
+  accepts_nested_attributes_for :config, update_only: true
+
+  validates :title, presence: true
+  validates :note, presence: true, allow_blank: true
+
+  validates :title, uniqueness: {case_sensitive: false}
+
+  validates :amount_cents, numericality: {greater_than_or_equal_to: 0}
+
+  validates :title, length: {maximum: 40}
+  validates :note, length: {maximum: 32}
+
+  def amount
+    Money.new(amount_cents * self.class.groups[group])
+  end
+end
---
+++ test/models/reminder_test.rb
@@ -0,0 +1,47 @@
+require "test_helper"
+
+class ReminderTest < ActiveSupport::TestCase
+  test "associations" do
+    reminder = Reminder.new
+
+    assert belong_to(:account).matches?(reminder)
+    assert belong_to(:category).matches?(reminder)
+  end
+
+  test "delegated types" do
+    reminder = Reminder.new
+
+    assert have_delegated_type(:config)
+      .types(%w[ReminderMonthlyConfig])
+      .matches?(reminder)
+  end
+
+  test "validations" do
+    reminder = create(:reminder)
+
+    assert validate_presence_of(:title).matches?(reminder)
+
+    assert validate_uniqueness_of(:title).case_insensitive.matches?(reminder)
+
+    assert validate_numericality_of(:amount_cents)
+      .is_greater_than_or_equal_to(0)
+      .matches?(reminder)
+
+    assert validate_length_of(:title).is_at_most(40).matches?(reminder)
+
+    assert validate_length_of(:note)
+      .is_at_most(32)
+      .allow_nil
+      .matches?(reminder)
+  end
+
+  test "#amount" do
+    reminder_a = Reminder.new(group: :expense, amount_cents: 1234)
+    reminder_b = Reminder.new(group: :income, amount_cents: 1234)
+    no_amount = Reminder.new(group: :expense, amount_cents: 0)
+
+    assert_equal Money.new(-12_34), reminder_a.amount
+    assert_equal Money.new(12_34), reminder_b.amount
+    assert no_amount.amount.zero?
+  end
+end
