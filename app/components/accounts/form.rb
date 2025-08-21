b96765fdb5baacb1255c28b50d3f6e4cc3e01c18
stephann <3025661+stephannv@users.noreply.github.com>
2025-08-21 19:08:15 -0300

    Refactor Bolt forms (#56)

--- app/components/accounts/form.rb
+++ app/components/accounts/form.rb
@@ -8,43 +8,42 @@ module Components
       private
 
       def view_template
-        form_with model: @account do |form|
+        Bolt.Form model: @account do |form|
           Bolt.Stack do
-            Bolt.ErrorSummary form
+            form.error_summary
 
             Bolt.Panel do
               Bolt.Stack do
-                Bolt.FieldWrapper do
-                  Bolt.Label form, :title
-                  Bolt.TextField form, :title
-                  Bolt.FieldError form, :title
+                form.field_wrapper do
+                  form.label :title
+                  form.text_field :title
+                  form.field_error :title
                 end
 
-                Bolt.FieldWrapper do
-                  Bolt.Label form, :color
-                  Bolt.ColorPicker form, :color
-                  Bolt.FieldError form, :color
+                form.field_wrapper do
+                  form.label :color
+                  form.color_picker :color
+                  form.field_error :color
                 end
 
-                Bolt.FieldWrapper do
-                  Bolt.Label form, :initial_amount_cents
-                  Bolt.MoneyField form, :initial_amount_cents,
-                    allow_negative: true
-                  Bolt.FieldError form, :initial_amount_cents
+                form.field_wrapper do
+                  form.label :initial_amount_cents
+                  form.money_field :initial_amount_cents, allow_negative: true
+                  form.field_error :initial_amount_cents
                 end
 
                 if @account.persisted?
-                  Bolt.FieldWrapper do
-                    Bolt.Label form, :archived
-                    Bolt.Toggle form, :archived
-                    Bolt.FieldError form, :archived
+                  form.field_wrapper do
+                    form.label :archived
+                    form.toggle :archived
+                    form.field_error :archived
                   end
                 end
               end
             end
 
             Bolt.Stack gap: :sm do
-              Bolt.Submit form
+              form.submit
 
               Bolt.LinkButton href: cancel_href, ghost: true do
                 Lucide.ArrowLeft(class: "size-5")
