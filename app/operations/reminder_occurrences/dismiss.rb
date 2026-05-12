6db35185b97f80b6a4353a448a95fb68befd9c7c
stephann <stephann@gunbolt.org>
2026-05-12 14:06:11 -0300

    Add dismiss reminder occcurrence operation (#63)

---
+++ app/operations/reminder_occurrences/dismiss.rb
@@ -0,0 +1,25 @@
+module ReminderOccurrences
+  class Dismiss < ApplicationOperation
+    prop :id, Id
+
+    Success = Result.define(occurrence: ReminderOccurrence)
+    Failure = Result.define(occurrence: ReminderOccurrence)
+
+    def call
+      occurrence = ReminderOccurrence.find(@id)
+
+      return Failure[occurrence:] if !occurrence.pending?
+
+      reminder = occurrence.reminder
+
+      ReminderOccurrence.transaction do
+        occurrence.dismissed!
+        reminder.occurrences.create!(occurs_on: reminder.next_occurrence_date)
+
+        Success[occurrence:]
+      rescue
+        Failure[occurrence:]
+      end
+    end
+  end
+end
